dcb570523b492d2c8f57e4cdf2cda06084216626
test <zhuxiaofeng@wudidaimengshuai.local>
2018-04-23 19:07:20 +0800

    流程基本接通 ->调整具体UI

--- IFenQu/IFenQu/ViewControllers/Shop/PurchaseViewController.swift
+++ IFenQu/IFenQu/ViewControllers/Shop/PurchaseViewController.swift
@@ -15,11 +15,18 @@ class PurchaseViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "购买确认"
-        contenScrollView = UIScrollView.init(frame: self.view.bounds)
-        
+        contenScrollView = UIScrollView.init()
+        contenScrollView.contentSize = CGSize.init(width: SCREEN_Width, height: 1000)
         self.view.backgroundColor = xlightGray
         contenScrollView.backgroundColor = xlightGray
         self.view.addSubview(contenScrollView)
+        
+        contenScrollView.mas_makeConstraints { (make) in
+            make?.right.equalTo()(self.view.mas_right)
+            make?.top.equalTo()(self.view.mas_top)
+            make?.bottom.equalTo()(self.view.mas_bottom)
+            make?.left.equalTo()(self.view.mas_left)
+        }
         
         let addressLable = UILabel.init(frame: CGRect.init(x: 20, y: 5, width: SCREEN_Width, height: 30))
         addressLable.text = "详细收货信息"
@@ -71,8 +78,23 @@ class PurchaseViewController: BaseViewController {
             make?.width.equalTo()(30)
         }
         bottomView.backgroundColor = UIColor.white
+        
+        contentInfoView = PurchaseContentView.initFormNib() as! PurchaseContentView
+        contenScrollView.addSubview(contentInfoView)
+        
+        contentInfoView.mas_makeConstraints { (make) in
+            make?.right.equalTo()(self.view.mas_right)
+            make?.top.equalTo()(addressView.mas_bottom)?.offset()(20)
+//            make?.height.equalTo()(500)
+            make?.bottom.equalTo()(contenScrollView.mas_bottom)
+            make?.left.equalTo()(self.view.mas_left)
+        }
+        
+        
+        
     }
-
+    var contentInfoView: PurchaseContentView!
+    
     lazy var bottomView = { () -> ConfirmView in
         let v = ConfirmView.initFormNib() as! ConfirmView
         v.frame = CGRect.init(x: 0, y: self.view.height - 80, width: SCREEN_Width, height: 80)
@@ -84,7 +106,7 @@ class PurchaseViewController: BaseViewController {
     var addreLableInfo: UILabel!
     var v: UIView!
     @objc func addressAction() {
-//        addreLableInfo.text = "追问发会为和giureiugnreugnriutgtirugnuirtgnrtjngirtngjrtjrnjgbjrtgjtrn"
+//        addreLableInfo.text = "追问发会为和giureiugnreugnriutgtirugnuirtgnrtjngirtngjrtjrnjgbjrtgjtrn追问发会为"
 //        v.mas_updateConstraints { (make) in
 //            make?.height.equalTo()((addreLableInfo.text?.getTextSizeB(font: addreLableInfo.font, size: CGSize.init(width: SCREEN_Width - 70, height: SCREEN_Height)).height)! + 30)
 //        }
---
+++ IFenQu/IFenQu/Views/Shop/PurchaseContentView.swift
@@ -0,0 +1,18 @@
+//
+//  PurchaseContentView.swift
+//  IFenQu
+//
+//  Created by 朱晓峰 on 2018/4/23.
+//  Copyright © 2018年 朱晓峰. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseContentView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = xlightGray
+    }
+    
+
+}
